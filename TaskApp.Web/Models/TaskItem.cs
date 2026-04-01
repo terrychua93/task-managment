7461c3da57c8896545f8ee0068c79fb46d097eaa
terrychua93 <terrychua93@gmail.com>
2026-04-01 23:17:09 +0800

    feat(web): add model for TaskItem

---
+++ TaskApp.Web/Models/TaskItem.cs
@@ -0,0 +1,10 @@
+namespace TaskApp.Web.Models;
+
+public class TaskItem
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = "";
+    public string? Description { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
