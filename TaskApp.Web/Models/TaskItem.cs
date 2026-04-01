4b6864480c93a0a8b1fc7b86048a91d7c858e925
terrychua93 <terrychua93@gmail.com>
2026-04-01 23:55:15 +0800

    feat: add validation and styling

--- TaskApp.Web/Models/TaskItem.cs
+++ TaskApp.Web/Models/TaskItem.cs
@@ -1,9 +1,15 @@
 namespace TaskApp.Web.Models;
+using System.ComponentModel.DataAnnotations;
 
 public class TaskItem
 {
     public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Title is required")]
+    [MinLength(3, ErrorMessage = "Title must be at least 3 characters")]
     public string Title { get; set; } = "";
+    [Required(ErrorMessage = "Description is required")]
+    [MinLength(3, ErrorMessage = "Description must be at least 6 characters")]
     public string? Description { get; set; }
     public bool IsCompleted { get; set; }
     public DateTime CreatedAt { get; set; }
